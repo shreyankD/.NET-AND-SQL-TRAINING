80068876aab81cbe628594a761d199f151b1c4da
Shreyank Devanand <shreyankd@infinite.com>
2023-09-08 10:07:02 +0530

    assignment

---
+++ assignment1/assignment1/Program1.cs
@@ -0,0 +1,31 @@
+﻿//program 1
+using System;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" enter first digit:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" enter second digit:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine(" The two digits are equal:");
+                Console.ReadLine();
+            }
+            else
+            {
+
+                Console.WriteLine(" The two digits are not equal");
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
+

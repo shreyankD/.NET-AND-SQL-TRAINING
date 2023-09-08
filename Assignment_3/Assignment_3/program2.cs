df37225d035ef2bd72fbea306508a55bdc9096ef
Shreyank Devanand <shreyankd@infinite.com>
2023-09-08 13:28:43 +0530

    assignment 3

---
+++ Assignment_3/Assignment_3/program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+namespace program2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the string ");
+
+            string userip = Console.ReadLine();
+
+            string reverse = revstr(userip);
+            Console.WriteLine($"reverse is {reverse}");
+            Console.ReadLine();
+        }
+
+       
+        static string revstr(string input)
+        {
+            StringBuilder reversed = new StringBuilder();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversed.Append(input[i]);
+            }
+
+            return reversed.ToString();
+        }
+    }
+}

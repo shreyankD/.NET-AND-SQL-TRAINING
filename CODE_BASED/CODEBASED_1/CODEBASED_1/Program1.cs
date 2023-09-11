3d7138a3dfd62064b27678f739abadd0b416fb36
Shreyank Devanand <shreyankd@infinite.com>
2023-09-11 10:57:42 +0530

    CODE_BASED_1

---
+++ CODE_BASED/CODEBASED_1/CODEBASED_1/Program1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CODEBASED_1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("enter the position");
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            if (position >= 0 && position < inputString.Length)
+            {
+                string resultString = RemoveCharacter(inputString, position);
+                Console.WriteLine("Result string: " + resultString);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("invalid position");
+            }
+
+
+        }
+
+         static string RemoveCharacter(string input, int position)
+        {
+            return input.Remove(position, 1);
+        }
+    }
+}
+

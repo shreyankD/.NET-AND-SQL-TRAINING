64bf3f19b89843977f3cf892b9865bbb8cbf9a48
shreyankD <144223976+shreyankD@users.noreply.github.com>
2023-09-14 15:30:13 +0530

    Add files via upload
    TRAIN_TICKET_BOOKING TASK

---
+++ Train_Ticket_Booking/Train_Ticket_Booking/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Booking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER YOUR NAME:");
+            Console.ReadLine();
+            Console.WriteLine("ENTER YOUR AGE: ");
+            Console.ReadLine();
+            TicketBooking ticketBooking = new TicketBooking();
+            try
+            {
+                ticketBooking.Ticket_Booking();
+            }
+            catch (ticketException tE)
+            {
+                Console.WriteLine(tE.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+    class ticketException : Exception
+    {
+        public ticketException(string message) : base(message)
+        {
+        }
+    }
+    class TicketBooking
+    {
+        public void Ticket_Booking()
+        {
+            Console.WriteLine("ENTER THE TOTAL NUMBER OF TICKETS : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 2)
+            {
+                throw (new ticketException("CANNOT BOOK MORE THAN 2 TICKETS "));
+            }
+            else
+            {
+                Console.WriteLine("BOOKED SUCCESSFULLY");
+            }
+        }
+    }
+}

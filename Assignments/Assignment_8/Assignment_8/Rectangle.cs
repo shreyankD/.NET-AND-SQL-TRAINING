f2017c14f0b373c0f5db66bd8de160d6a4f0e1c6
shreyankD <144223976+shreyankD@users.noreply.github.com>
2023-09-26 21:18:03 +0530

    Add files via upload

---
+++ Assignments/Assignment_8/Assignment_8/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8
+{
+   
+    class Rectangle : IShapes
+    {
+        double LENGTH;
+        double BREADTH;
+
+       public Rectangle()
+        {
+            Console.WriteLine("Enter the Length of Rectangle:");
+            LENGTH = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Breadth of Rectangle:");
+            BREADTH= Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double GetArea()
+        {
+            return LENGTH*BREADTH;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * (LENGTH + BREADTH);
+        }
+    }
+}
---
+++ Assignments/Assignment_8/Assignment_8/ShapeFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8
+{
+    public class ShapeFactory
+    {
+        public static IShapes GetShapes(string typesofshape)
+        {
+            IShapes ishapes = null;
+            if(typesofshape == "rectangle")
+            {
+                ishapes = new Rectangle();
+            }
+            else if (typesofshape == "circle")
+            {
+                ishapes = new Circle();
+            }
+            else if ( typesofshape == "triangle")
+            {
+                ishapes = new Triangle();
+            }
+            else if (typesofshape == "square")
+            {
+                ishapes = new Square();
+            }
+            return ishapes;
+        }
+    }
+}

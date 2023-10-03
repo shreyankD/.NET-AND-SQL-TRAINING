5f89cc87c26bb8f27ffc9021e1dc348bc71bd5ef
shreyankD <144223976+shreyankD@users.noreply.github.com>
2023-10-03 18:09:42 +0530

    Update Program1.cs

--- CODE_BASED/CODEBASED_1/CODEBASED_4/CODEBASED_4/Program1.cs
+++ CODE_BASED/CODEBASED_1/CODEBASED_4/CODEBASED_4/Program1.cs
@@ -11,41 +11,17 @@ namespace CODEBASED_4
         {
             static void Main()
             {
-
                 string filePath = @"D:\csharp_assignments\CODE_BASED\CODEBASED_4\CODEBASED_4\CODEBASED_Sample_text.txt";
-
-
-
-                // message to text file
-
                 string textToAppend = "this is a sample text for code based Test_4";
 
-
-
-                // for appending
-
                 using (StreamWriter writer = new StreamWriter(filePath, true))
-
                 {
-
-
                     writer.WriteLine(textToAppend);
-
-
-
-
-
                     writer.Close();
-
                 }
 
-
-
                 Console.WriteLine("File has been Created and Text has been appended to that particular file.");
-
                 Console.ReadLine();
-
             }
-
         }
     }

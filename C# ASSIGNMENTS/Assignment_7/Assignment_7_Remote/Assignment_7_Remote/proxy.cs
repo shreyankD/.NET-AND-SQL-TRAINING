c6fee4caba198a426a6fc182b3d1d2759c2f2dc1
shreyankD <144223976+shreyankD@users.noreply.github.com>
2023-10-07 22:06:29 +0530

    Add files via upload

---
+++ C# ASSIGNMENTS/Assignment_7/Assignment_7_Remote/Assignment_7_Remote/proxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7_Remote
+{
+    public class YourRemoteObject : MarshalByRefObject
+
+    {
+
+        public string HelloWorld()
+
+        {
+
+            return "Hello World from Remote Object!";
+              
+        }
+    }
+}
---
+++ C# ASSIGNMENTS/Assignment_7/Assignment_7_Windows/Assignment_7_Windows/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using Assignment_7_Remote;
+
+namespace Assignment_7_Windows
+{
+    
+    public partial class Form1 : Form
+
+    {
+
+        public Form1()
+
+        {
+
+            InitializeComponent();
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+
+            TcpChannel channel = new TcpChannel();
+
+            ChannelServices.RegisterChannel(channel, false);
+
+
+
+            YourRemoteObject remoteObject =
+
+                (YourRemoteObject)Activator.GetObject(
+
+                    typeof(YourRemoteObject),
+
+                    "tcp://localhost:8080/RemoteObject");
+
+
+
+            string result = remoteObject.HelloWorld();
+
+            label1.Text = result;
+
+        }
+
+    }
+}
+

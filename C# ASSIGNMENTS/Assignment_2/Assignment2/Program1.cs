200010b27aed9ff02a9ebb840af1ba92b39d9689
shreyankD <144223976+shreyankD@users.noreply.github.com>
2023-10-07 21:58:41 +0530

    Add files via upload

---
+++ C# ASSIGNMENTS/Assignment_2/Assignment2/Program1.cs
@@ -0,0 +1,44 @@
+﻿//first program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class program1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of elements in the array :");
+
+        int count = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[count];
+        Console.WriteLine("Enter the Elements of the Array : ");
+        for (int i = 0; i < count; i++)
+        {
+           numbers[i]=Convert.ToInt32(Console.ReadLine());
+        }
+        int sum = 0;
+        int min = numbers[0];
+        int max = numbers[0];
+        
+        foreach(int num in numbers)
+        {
+            sum += num;
+            if(num<min)
+            {
+                min = num;
+            }
+            if(num>max)
+            {
+                max = num;
+            }
+        }
+        int average = sum / count;
+        Console.WriteLine("average:" + average);
+        Console.ReadLine();
+        Console.WriteLine("Minimum:" + min);
+        Console.ReadLine();
+        Console.WriteLine("Maximum:" + max);
+        Console.ReadLine();
+
+    }
+}
---
+++ C# ASSIGNMENTS/Assignment_4/Assignment_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,75 @@
+<<<<<<< HEAD
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assignment_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment_4")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21b0a9f8-ba6d-4263-a0df-1b69355d5a63")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+=======
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assignment_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment_4")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21b0a9f8-ba6d-4263-a0df-1b69355d5a63")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+>>>>>>> 46a3b041368c6487bb241fdc9b2f6a71ee11e10e
